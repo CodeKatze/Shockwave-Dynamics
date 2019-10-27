a754da16cd4ed000298f4e2b08127eabec1ddaf8
Pandora <ananthunairs123@gmail.com>
2019-10-28 03:01:51 +0530

    Partially Finished Transitions

--- 5 - Transition/Transition.m
+++ 5 - Transition/Transition.m
@@ -1,11 +1,24 @@
-gamma = 1.4;
-M0 = 2;
+p0 = 1;
+g = 1.4;
 
-phi1 = linspace(asind(1/M0),90,100);
-[M1, ~, ~, ~, theta1] = Shockfun(M0,phi1,gamma);
-for m = M1
-    theta2max = thetaMax(m,gamma)
+M0 = 1:0.1:5;
+phi_detach = [];
+phi_vn = [];
+for m = M0
+    guess = [1 , 1, 1, 1, 0.1, 0.1, 0.1 ,0.1];
+    options=optimset('TolFun',.001,'MaxFunEvals',100000,'MaxIter',100000);
+    x = fsolve(@(X)eqns_Detach(X,m,p0,g),guess,options);
+    phi_detach = [phi_detach real(x(7))];
 end
+plot(M0,phi_detach);
+hold on;
+for m = M0
+    guess = [1 , 1, 1, 1, 1, 1, 0.1, 0.1, 0.1, 0.1 ,0.1, 0.1];
+    options=optimset('TolFun',.001,'MaxFunEvals',10000000,'MaxIter',10000000);
+    x = fsolve(@(X)eqns_Vn(X,m,p0,g),guess,options);
+    phi_vn = [phi_vn real(x(10))];
+end
+plot(M0,phi_vn);
 % phi = phifromtheta(m,theta_w,gamma);
 
 % [Mj, ~, ~, ~, thetaj] = Shockfun(m,phi,gamma);
